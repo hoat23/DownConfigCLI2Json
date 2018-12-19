1e3d9fb77e63b0778d323b0597db958ef46035cb
Deiner Zapata <deinerzapata@gmail.com>
2018-12-18 23:31:02 -0500

    Bat python

---
+++ exec_python.bat
@@ -0,0 +1,6 @@
+
+@echo off
+echo Downloading config of firewall...
+python /usr/share/logstash/downConfigFireWallSHH.py  -i 8.8.8.8 -pp 5000 -u miuser -p p@ssword -c "down_config" -ip_out 9.9.9.9 -pp_out 4000
+cd
+echo fin
